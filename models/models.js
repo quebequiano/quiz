b2e21e8644bd82a8d65a8c27ff07a4f3986011c8
quebequiano@gmail.com <quebequiano@gmail.com>
2015-07-28 19:43:53 +0200

    modulo7

--- models/models.js
+++ models/models.js
@@ -32,8 +32,9 @@ exports.Quiz=Quiz;
 sequelize.sync().success(function(){
    Quiz.count().success(function(count){
       if(count===0){
-	Quiz.create({pregunta:'Capital de Italia',respuesta:'Roma'}).success(function(){console.log('Base de datos inicializada')});
-	
+	Quiz.create({pregunta:'Capital de Italia',respuesta:'Roma'});
+	Quiz.create({pregunta:'Capital de Portugal',respuesta:'Lisboa'})
+	.then(function(){console.log('Base de datos inicializada')});
       };
    });
 }); 
